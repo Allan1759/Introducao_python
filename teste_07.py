d806a39d4e5e497a7b98b55dad573e141d621ed8
Allan1759 <allan.uchoa.1756@gmail.com>
2025-02-13 17:37:30 -0300

    Add files via upload

---
+++ teste_07.py
@@ -0,0 +1,15 @@
+#EXERCICIOS
+
+# Escreva um programa que use a função range() para gerar os números pares de 2 a 20 e
+# em seguida, imprima cada número.
+
+lista = list(range(2,21 , 2))
+print(lista)
+
+# Crie uma lista chamada carros contendo três nomes de carros diferentes. 
+# Em seguida, concatene essa lista com a lista numeros e imprima o resultado.
+
+num1 = [1,2,3,4,5]
+carros = ['corsa','celta','gol']
+print(num1+carros)
+
