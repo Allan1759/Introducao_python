0c1bdd5a3737e8dcb45d1d5a4e3154cca93fb5db
Allan Leonardo <allan.uchoa.1756@gmail.com>
2025-02-18 01:00:21 -0300

    Add files via upload

---
+++ Aula_input_output.py
@@ -0,0 +1,66 @@
+#mensagem = ('Seja bem vindo!!')
+#print(mensagem)
+
+#-----------------------------------------
+
+#Var1 = True
+#print(Var1)
+#print(type(Var1))
+
+#-----------------------------------------
+
+#Num1 = float(input('Insira o primeiro numero'))
+#Num2 = float(input('Insira o segundo numero'))
+
+#print('O resultado da multiplicação entre os numeros {} e {} é {:.2f}'.format(Num1,Num2,Num1*Num2))
+
+#-----------------------------------------
+
+#Num1 = int(input('Insira o primeiro numero: '))
+#Num2 = int(input('Insira o segundo numero: '))
+#print('O resultado da divisão entre os numeros {} e {} é {}'.format(Num1,Num2,Num1/Num2))
+
+#------------------------------------------
+
+#Num1 = int(input('Insira o primeiro numero: '))
+#Num2 = int(input('Insira o segundo numero: '))
+#print('O resultado da subtração entre os numeros {} e {} é igual a {}'.format(Num1,Num2,Num1-Num2))
+
+#------------------------------------------
+
+#Num1 = int(input('Insira o primeiro numero: '))
+#Num2 = int(input('Insira o segundo numero: '))
+
+#print('O resultado da divisão entre os numeros {} e {} é igual a {}'.format(Num1,Num2,Num1/Num2))
+
+#------------------------------------------
+
+#Num1 = float(input('Insira o primeiro numero: '))
+#Num2 = float(input('Insira o segundo numero: '))
+#Num3 = float(input('Insira o terceiro numero: '))
+#Num4= float(input('Insira o quarto numero: '))
+
+#print('O resultado da multiplicação de todos os elementos é igual a {:.1f}'.format(Num1*Num2*Num3*Num4))
+
+#-----------------------------------------
+
+#Num1 = int(input('Insira um numero: '))
+#print('O dobro de {} é igual a {}'.format(Num1,Num1*2))
+
+#-----------------------------------------
+
+#Num1 = float(input('Insira o primeiro numero: '))
+#Num2 = float(input('Insira o segundo numero: '))
+#print('O resultado da soma entre os numeros {} e {} é igual a {:.2f}'.format(Num1,Num2,Num1+Num2))
+
+#-----------------------------------------
+
+#cad_1 = input('Insira seu nome completo: ')
+#print('Seja bem vindo',cad_1)
+#print('Cadastro realizado com sucesso')
+
+x = True
+
+print(x)
+
+
