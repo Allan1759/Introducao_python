eaaac83d0cfb117679daf38dc752c214ae8bd45f
Allan Leonardo <allan.uchoa.1756@gmail.com>
2025-02-19 17:19:13 -0300

    Add files via upload

---
+++ Aula_funcao.py
@@ -0,0 +1,48 @@
+#CRIE UM AFUNÇÃO PARA MULTIPLICAR 3 NUMEROS.
+
+# def multi(): 
+#     n1 = float(input('Insira o primeiro numero: '))
+#     n2 = float(input('insira o segundo numero: '))
+#     n3 = float(input('Insira o terceiro numero: '))
+#     multi = n1 * n2 * n3
+#     print(multi)
+
+# multi()
+
+
+#CRIE UMA FUNÇÃO PARA DESCOBRIR O VALOR ELEVADO DE UM NÚMERO.
+
+# def potencia ():
+#     n1 = float(input('Insira o valor a ser elevado: '))
+#     potencia = n1 ** 2
+#     print(potencia)
+# potencia()
+
+
+#CRIE UMA FUNÇÃO PARA MOSTRAR UMA MENSAGEM PERSONALIZADA NA TELA, SE O USUÁRIO  DIGITAR, 18 ANOS
+
+# def mensagem():
+#     idade = int(input('Digite sua idade: '))
+#     if idade >= 18:
+#         print('Parabens, você já é um adulto.')
+#     elif idade < 18:
+#         print('Você ainda não tem acesso, espere completar 18 anos.')
+# mensagem()
+
+
+# def idade():
+#     nasci = int(input('digite o ano de nascimento: '))
+#     id_1 = (2025 - nasci)
+#     print(id_1)
+# idade()
+
+#DESENVOLVA UM AFUNÇÃO PARA VER SE O BRASIL GANHOU A COPA DE 1999
+
+# def copa():
+#     print('COPA AMERICA DE 1999 \nCOPA DO MUNDO DE 1999 ')
+#     copa1 = input('Insira qual das copas gostaria de saber: ')
+#     if copa1 == 'copa america':
+#         print('Sim, o Brasil ganhou a copa america de 1999')
+#     elif copa1 == 'copa do mundo':
+#         print('Não houve copa do mundo neste ano ')
+# copa()
